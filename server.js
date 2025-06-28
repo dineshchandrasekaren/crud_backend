69b3f372156f85f5384aee44d8bdda9a967b5797
dineshchandrasekaren <dineshchandrasekaren@gmail.com>
2025-06-28 16:48:34 +0530

    updated

--- server.js
+++ server.js
@@ -14,10 +14,11 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: process.env.CORS_ORIGIN || "http://localhost:3000",
-    credentials: true,
+    origin: "*",
+    credentials: false,
   })
 );
+
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 
